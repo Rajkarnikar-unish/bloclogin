9a76e556b7a8e9f1cc43eaea128d2767bbc4173e
Rajkarnikar-unish <ubidy947@gmail.com>
2022-09-11 21:59:11 -0500

    feat: bloc and models added

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,6 @@
 import 'package:authentication_repository/authentication_repository.dart';
 import 'package:bloclogin/authentication/bloc/authentication_bloc.dart';
+import 'package:bloclogin/splash/splash.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:user_repository/user_repository.dart';
@@ -29,11 +30,46 @@ class App extends StatelessWidget {
   }
 }
 
-class AppView extends StatelessWidget {
+class AppView extends StatefulWidget {
   const AppView({super.key});
 
   @override
+  State<AppView> createState() => _AppViewState();
+}
+
+class _AppViewState extends State<AppView> {
+  final _navigatorKey = GlobalKey<NavigatorState>();
+
+  NavigatorState get _navigator => _navigatorKey.currentState!;
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      navigatorKey: _navigatorKey,
+      builder: (context, child) {
+        return BlocListener<AuthenticationBloc, AuthenticationState>(
+          listener: (context, state) {
+            switch (state.status) {
+              case AuthenticationStatus.authenticated:
+                _navigator.pushAndRemoveUntil(
+                  HomePage.route,
+                  (route) => false,
+                );
+                break;
+              case AuthenticationStatus.unauthenticated:
+                _navigator.pushAndRemoveUntil(
+                  LoginPage.route(),
+                  (route) => false,
+                );
+                break;
+              case AuthenticationStatus.unknown:
+                break;
+            }
+          },
+          child: child,
+        );
+      },
+      onGenerateRoute: (_) => SplashPage.route(),
+    );
   }
 }
---
+++ lib/login/login.dart
@@ -0,0 +1,4 @@
+export 'models/models.dart';
+export 'models/username.dart';
+export 'models/password.dart';
+export 'bloc/login_bloc.dart';
